411dc9f74cddb2070a851efaff07b6c83987b832
suhasmaddali <suhas.maddali.chinnu@gmail.com>
2024-06-20 15:07:50 -0700

    Modified the test.py file to successfully train models with neural networks

--- src/test.py
+++ src/test.py
@@ -16,8 +16,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.svm import SVR
 from datetime import datetime
-# from tensorflow.keras.layers import Dense, Activation
-# from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation
+from tensorflow.keras.models import Sequential
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -61,10 +61,28 @@ if __name__ == "__main__":
     df['day'] = df['day'].apply(lambda x: int(x))
     df.drop(['casual', 'registered'], axis = 1, inplace = True)
     df['dteday'] = df['dteday'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d'))
+    df.drop(['instant', 'dteday'], axis = 1, inplace = True)
 
     print(df.head())
 
     X = df.drop(['cnt'], axis = 1).values
     y = df['cnt'].values
 
+    X_train, X_cv, y_train, y_cv = train_test_split(X, y, test_size = 0.3, random_state = 101)
+
+    scaler = MinMaxScaler()
+    scaler.fit(X_train)
+    X_train = scaler.transform(X_train)
+    X_cv = scaler.transform(X_cv)
+
+    model = Sequential()
+    model.add(Dense(100, activation = 'relu'))
+    model.add(Dense(50, activation = 'relu'))
+    model.add(Dense(25, activation = 'relu'))
+    model.add(Dense(10, activation = 'relu'))
+    model.add(Dense(5, activation = 'relu'))
+    model.add(Dense(1))
+    model.compile(optimizer = 'adam', loss = 'MSE', metrics = ['MSE', 'MAE'])
+
+    model.fit(X_train, y_train, epochs = 10, verbose = 1, validation_data = (X_cv, y_cv))
 
