1c9e53ae1a4b6a839e4ad51da7ce252557818ae5
suhasmaddali <suhas.maddali.chinnu@gmail.com>
2024-06-20 14:28:54 -0700

    Added docker-compose file to build containers

--- test.py
+++ test.py
@@ -1,1 +1,23 @@
+import sys
+import pprint
+import seaborn as sns
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import warnings
+from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import mean_squared_error, mean_absolute_error, roc_auc_score
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.cross_decomposition import PLSRegression
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.svm import SVR
+from tensorflow.keras.layers import Dense, Activation
+from tensorflow.keras.models import Sequential
+import warnings
+warnings.filterwarnings("ignore")
+
 print("Hello World")
